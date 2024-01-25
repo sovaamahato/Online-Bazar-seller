68c47bd3980e5a7960da54b53fbacbcc649310ce
sova <mahatoshobha143@gmail.com>
2024-01-25 22:34:19 +0545

    initial setup

---
+++ android/app/src/main/kotlin/com/example/online_bazar_seller/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.online_bazar_seller
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/const/strings.dart
@@ -0,0 +1,4 @@
+const appname = "Online Bazar Seller App",
+    welcome = "Hi, Welcome to",
+    anyProblem = "In case of any difficulty, contact administration",
+    credit = "@SovaKushwaha";
