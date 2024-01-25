a45e4eb0575c87ae5940a188b1fed3d30e41dd24
sova <mahatoshobha143@gmail.com>
2024-01-25 23:47:07 +0545

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'views/home_screen/home_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyDhv5E9Go1ZlCiRziPfb73wWUnsScKjTZI",
+      appId: "1:114827691684:android:df2f8af97a13a11b878831",
+      messagingSenderId: "114827691684",
+      projectId: "online-bazar-b5ca8",
+    ),
+  );
+
   runApp(const MyApp());
 }
 
@@ -16,6 +29,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/views/home_screen/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:online_bazar_seller/const/const.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: "Hello".text.color(purpleColor).make()),
+      body: Container(color: red),
+    );
+  }
+}
